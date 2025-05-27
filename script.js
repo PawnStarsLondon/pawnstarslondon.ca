08f4aef9dc4bb7a9455e6dc8d7e34632e0da5391
unknown <christian@rootevolution.com>
2025-05-27 01:19:58 -0400

    Add photo gallery page with GLightbox functionality and categories

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // JavaScript for interactivity will go here 
+
+// GLightbox initialization
+document.addEventListener('DOMContentLoaded', () => {
+    const lightbox = GLightbox({
+        selector: '.glightbox', // Targets all links with the class 'glightbox'
+        touchNavigation: true,
+        loop: true, // Set to true if you want the gallery to loop
+        autoplayVideos: false,
+        // You can add more options here as needed, e.g., for descriptions, titles
+        // See GLightbox documentation for all options: https://glightbox.mcstudios.com.mx/
+    });
+});
+
+// Any other future JavaScript for interactivity can go here 
